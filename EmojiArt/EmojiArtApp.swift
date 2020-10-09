c775280630b64d9fc65d025deb9c227c41f02c9b
Bogdan Orzea <bogdan.orzea@gmail.com>
2020-10-08 21:44:02 -0400

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Bogdan Orzea on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
